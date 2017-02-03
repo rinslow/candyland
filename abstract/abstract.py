a6fd596d7dbd901cff9f644ec2b5a3289cac7c7a
d.kiss <speakupness@gmail.com>
2017-02-03 11:46:35 +0200

    Added List support

--- abstract/abstract.py
+++ abstract/abstract.py
@@ -29,6 +29,9 @@ class Queue(Immutable):
     def __len__(self):
         return len(self._list)
 
+    def __str__(self):
+        return str(self._list)
+
 
 class Stack(Immutable):
     """Immutable stack.
@@ -55,3 +58,73 @@ class Stack(Immutable):
 
     def __len__(self):
         return len(self._list)
+
+    def __str__(self):
+        return str(self._list)
+
+
+class List(Immutable):
+    """Immutable list.
+
+    Attributes:
+        _list (list): list's implementation.
+    """
+    def __init__(self, _list=NotImplemented, *args):
+        super(List, self).__init__()
+        if type(_list) is int:
+            self._list = [_list] + list(args)
+
+        else:
+            self._list = _list if _list is not NotImplemented else []
+
+    def append(self, p_object):
+        return List(self._list + [p_object])
+
+    def count(self, value):
+        return self._list.count(value)
+
+    def index(self, value, start=None, stop=None):
+        for var, index in enumerate(self._list[start:stop]):
+            if var == value:
+                return index
+
+    def insert(self, index, p_object):
+        return List(self._list[:index] + [p_object] + self._list[index:])
+
+    def pop(self, index=None):
+        index = index if index is not None else 0
+        return List([item for (idx, item) in enumerate(self._list) if idx !=
+                     index])
+
+    def remove(self, value):
+        return List([item for item in self._list if item != value])
+
+    def extend(self, iterable):
+        return List(self._list + iterable)
+
+    def reverse(self):
+        return List(self._list[::-1])
+
+    def sort(self, compareable=None, key=None, reverse=False):
+        return List(sorted(self._list), compareable, key, reverse)
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __add__(self, other):
+        return List(self._list + list(other))
+
+    def __mul__(self, other):
+        return List(self._list * other)
+
+    def __contains__(self, item):
+        return item in self._list
+
+    def __getitem__(self, item):
+        return self._list[item]
+
+    def __len__(self):
+        return len(self._list)
+
+    def __str__(self):
+        return str(self._list)
--- abstract/abstract_unittests.py
+++ abstract/abstract_unittests.py
@@ -1,9 +1,16 @@
-from abstract import Queue, Stack
+from abstract import Queue, Stack, List
 
 import unittest
 
 
 class QueueTest(unittest.TestCase):
+
+    def test_different_inits_equal(self):
+        q1 = Queue(1, 2)
+        q2 = Queue().enqueue(1).enqueue(2)
+
+        self.assertEqual(q1, q2)
+
     def test_init_empty(self):
         queue = Queue()
         self.assertEqual(len(queue), 0)
@@ -54,6 +61,92 @@ class StackTest(unittest.TestCase):
         stack = Stack(1, 2, 3)
         self.assertEqual(len(stack), 3)
 
-    def test_init_queue(self):
+    def test_init_list(self):
         stack = Stack([1, 2, 3])
         self.assertEqual(len(stack), 3)
+
+
+class ListTest(unittest.TestCase):
+    def test_init_empty(self):
+        l = List()
+        self.assertEqual(len(l), 0)
+
+    def test_init_lot_of_numbers(self):
+        l = List(1, 2, 3)
+        self.assertEqual(len(l), 3)
+
+    def test_init_list(self):
+        l = List([1, 2, 3])
+        self.assertEqual(len(l), 3)
+
+    def test_append(self):
+        list1 = List().append(1).append(2)
+
+        list2 = List(1, 2)
+        self.assertEqual(list1, list2)
+
+    def test_count(self):
+        list1 = List(1, 2, 1, 4)
+        self.assertEqual(list1.count(1), 2)
+
+    def test_index(self):
+        list1 = List(1, 2, 1, 4)
+        self.assertEqual(list1.index(2), 1)
+
+    def test_insert(self):
+        list1 = List(1, 2, 3)
+        expected_list = List(1, 7, 2, 3)
+        actual_list = list1.insert(1, 7)
+
+        self.assertEqual(expected_list, actual_list)
+
+    def test_pop(self):
+        list1 = List(1, 2, 3)
+        expected_list = List(1, 3)
+        actual_list = list1.pop(1)
+
+        self.assertEqual(expected_list, actual_list)
+
+    def test_pop_empty(self):
+        list1 = List(1, 2, 3)
+        expected_list = List(2, 3)
+        actual_list = list1.pop()
+
+        self.assertEqual(expected_list, actual_list)
+
+    def test_remove(self):
+        actual_list = List(1, 2, 3, 2, 2).remove(2)
+        expected_list = List(1, 3)
+
+        self.assertEqual(actual_list, expected_list)
+
+    def test_extend(self):
+        actual_list = List(1, 2).extend([1, 2])
+        self.assertEqual(actual_list, List(1, 2, 1, 2))
+
+    def test_reverse(self):
+        self.assertEqual(List(1, 2, 3).reverse(), List(3, 2, 1))
+
+    def test_sort(self):
+        self.assertEqual(List(1, 2, 3, 0).sort(), List(0, 1, 2, 3))
+
+    def test_iter(self):
+        self.assertEqual(list(iter(List(1, 2, 3))), list(iter([1, 2, 3])))
+
+    def test_add(self):
+        self.assertEqual(List(1, 2) + List(3), List(1, 2, 3))
+
+    def test_mul(self):
+        self.assertEqual(List(1, 2) * 2, List(1, 2, 1, 2))
+
+    def test_in(self):
+        self.assertTrue(1 in List(2, 1, 2))
+
+    def test_get_item(self):
+        self.assertEqual(List(2, 1, 3)[2], 3)
+
+    def test_len(self):
+        self.assertEqual(len(List(2, 1, 3)), 3)
+
+    def test_str(self):
+        self.assertEqual(str(List(2, 1, 3)), str([2, 1, 3]))
