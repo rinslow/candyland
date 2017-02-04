dd9e86305489b906856aeef5737ca1d4e289a681
d.kiss <speakupness@gmail.com>
2017-02-04 18:14:11 +0200

    Finished implementing set.

--- abstract/abstract.py
+++ abstract/abstract.py
@@ -274,3 +274,70 @@ class Dictionary(Immutable):
     def pop(self, k):
         return Dictionary(dict([(key, value) for (key, value) in
                                 self._dict.items() if key != k]))
+
+
+class Set(Immutable):
+    """Immutable set.
+
+    Attributes:
+        _list (list): set's implementation.
+    """
+    def __init__(self, _list=NotImplemented, *args):
+        super(Set, self).__init__()
+        if type(_list) not in COLLECTIONS and _list is not NotImplemented:
+            self._list = list(set([_list] + list(args)))
+
+        else:
+            self._list = list(set(copy.copy(_list)
+                                  if _list is not NotImplemented else []))
+
+    def append(self, p_object):
+        return Set(list(set(self._list + [p_object])))
+
+    def count(self, value):
+        return self._list.count(value)
+
+    def index(self, value, start=None, stop=None):
+        for var, index in enumerate(self._list[start:stop]):
+            if var == value:
+                return index
+
+    def insert(self, index, p_object):
+        return Set(list(set(self._list[:index] +
+                            [p_object] +
+                            self._list[index:])))
+
+    def pop(self, index=None):
+        index = index if index is not None else 0
+        return Set([item for (idx, item) in enumerate(self._list)
+                    if idx != index])
+
+    def remove(self, value):
+        return Set([item for item in self._list if item != value])
+
+    def extend(self, iterable):
+        return Set(list(set(self._list + iterable)))
+
+    def reverse(self):
+        return Set(self._list[::-1])
+
+    def sort(self, compareable=None, key=None, reverse=False):
+        return Set(sorted(self._list), compareable, key, reverse)
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __add__(self, other):
+        return Set(list(set(self._list + list(other))))
+
+    def __contains__(self, item):
+        return item in self._list
+
+    def __getitem__(self, item):
+        return self._list[item]
+
+    def __len__(self):
+        return len(self._list)
+
+    def __str__(self):
+        return str(self._list)
